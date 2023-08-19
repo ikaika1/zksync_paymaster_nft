bc6fd835ec7e24b605618ada658e16a42c0c8b2d
ikaika1 <yutokeitoy@gmail.com>
2023-08-20 04:50:43 +0900

    first commit

---
+++ app/constants/consts.tsx
@@ -0,0 +1,9 @@
+export const GREETER_CONTRACT_ABI =
+  require("../../../zksync/artifacts-zk/contracts/Greeter.sol/Greeter.json").abi;
+export const NFT_CONTRACT_ABI =
+  require("../../../zksync/artifacts-zk/contracts/ERC721.sol/InfinityStones.json").abi;
+export const NETWORK_NAME = "zkSync Era Mainnet";
+export const NETWORK_ID = "0x144";
+export const GREETER_ADDRESS = "0x61F15cc3aFb33cc019913AC9D99b79727Ccef3D9";
+export const NFT_CONTRACT_ADDRESS = "0xD09392157817258EbEd4e449d1132443A60B3F68";
+export const PAYMASTER_CONTRACT_ADDRESS = "0x5fE9732ABE699870238B8f5C08A053A90e31d77e";
